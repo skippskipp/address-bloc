f5f49626110acb60ea821997c36f5785e2ae9188
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-04-22 14:57:23 -0400

    Assignment4-models complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,13 @@ class AddressBook
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name == entry.name
+        entries.delete(entry)
+      end
+    end
+  end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe AddressBook do
   end
 
   describe "#add_entry" do
-    it "adds only one entry tot he address book" do
+    it "adds only one entry to the address book" do
       book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
@@ -34,6 +34,31 @@ RSpec.describe AddressBook do
       expect(new_entry.name).to eq('Ada Lovelace')
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
+
+  describe "#remove_entry" do
+    it "removes only one entry from the address book" do
+      #add an entry, remove it. confirm that no entries are present
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(0)
+    end
+
+    it "removes the correct entry from the address book" do
+      #add two entries to the book, remove one. check that the correct entry was removed by seeing that index[0] matches second entry
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('James Lovelace', '011.012.1815', 'james.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+      new_entry = book.entries[0]
+
+      expect(new_entry.name).to eq('James Lovelace')
+      expect(new_entry.phone_number).to eq('011.012.1815')
+      expect(new_entry.email).to eq('james.king@lovelace.com')
+
     end
   end
 end
